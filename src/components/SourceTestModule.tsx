a2c9acd38809cc9aeabede290ce953250bfac741
SzeMeng76 <enquiry@ikmfoundation.org>
2025-12-28 17:02:29 +0800

    Revert "feat(loader): add CinematicLoader with purple theme"
    This reverts commit ce270230cb63498b08b03a63890ba70ef284ac6f.

--- src/components/SourceTestModule.tsx
+++ src/components/SourceTestModule.tsx
@@ -15,7 +15,6 @@ import { createPortal } from 'react-dom';
 
 import { SearchResult } from '@/lib/types';
 
-import CinematicLoader from '@/components/CinematicLoader';
 import VideoCard from '@/components/VideoCard';
 
 // 自定义优雅的 Select 组件
@@ -995,12 +994,6 @@ export default function SourceTestModule() {
           <p className='text-gray-600 dark:text-gray-400'>正在加载源列表...</p>
         </div>
       )}
-
-      {/* 批量测试加载动画 */}
-      <CinematicLoader
-        visible={isTestingAll}
-        message="Testing All Sources..."
-      />
     </div>
   );
 }
