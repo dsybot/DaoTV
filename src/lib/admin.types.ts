64be3e96f114a378cb7640254126be95e5daa7cb
dotSlash <freefrank21@gmail.com>
2026-01-10 22:09:53 -0700

    fix: add isTvBox field to AdminConfig type definition

--- src/lib/admin.types.ts
+++ src/lib/admin.types.ts
@@ -80,6 +80,7 @@ export interface AdminConfig {
     url: string;  // m3u 地址
     ua?: string;
     epg?: string; // 节目单
+    isTvBox?: boolean;
     from: 'config' | 'custom';
     channelNumber?: number;
     disabled?: boolean;
