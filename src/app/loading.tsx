e87097337e1f575c95cd37e26ccddc6cbe5ddf64
dsjbot <dsjbotcn@gmail.com>
2026-06-04 16:46:29 +0800

    fix: remove root app loading fallback

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { CinematicLoadingFallback } from '@/components/CinematicLoadingFallback';
-
-export default function Loading() {
-  return (
-    <div className='fixed inset-0 z-50'>
-      <CinematicLoadingFallback />
-    </div>
-  );
-}
